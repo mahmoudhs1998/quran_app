0dba1688bbb8580636a4e10ad813425189900a50
mahmoud <hmahmoud250@gmail.com>
2023-02-03 17:03:01 +0200

    new

--- lib/constant.dart
+++ lib/constant.dart
@@ -10,14 +10,14 @@ int bookmarkedSura = 1;
 bool fabIsClicked = true;
 
 final ItemScrollController itemScrollController = ItemScrollController();
-final ItemPositionsListener itemPositionsListener =ItemPositionsListener.create();
-
+final ItemPositionsListener itemPositionsListener =
+    ItemPositionsListener.create();
 
 String arabicFont = 'quran';
 double arabicFontSize = 28;
 double mushafFontSize = 40;
 
-Uri quranAppurl = Uri.parse('https://github.com/itsherifahmed');
+Uri quranAppurl = Uri.parse('https://github.com/mahmoudhs1998');
 
 Future saveSettings() async {
   final prefs = await SharedPreferences.getInstance();
@@ -36,8 +36,8 @@ Future getSettings() async {
   }
 }
 
-saveBookMark (surah,ayah)async{
-  final prefs=await SharedPreferences.getInstance();
+saveBookMark(surah, ayah) async {
+  final prefs = await SharedPreferences.getInstance();
   await prefs.setInt("surah", surah);
   await prefs.setInt("ayah", ayah);
 }
@@ -288,15 +288,15 @@ List<int> noOfVerses = [
   6
 ];
 
+List arabic = [];
+List malayalam = [];
+List quran = [];
 
-List arabic =[];
-List malayalam =[];
-List quran =[];
-
-Future readJson ()async{
-  final String response =await rootBundle.loadString("assets/hafs_smart_v8.json");
-  final data=json.decode(response);
-  arabic=data["quran"];
-  malayalam=data["malayalam"];
-  return quran=[arabic,malayalam];
+Future readJson() async {
+  final String response =
+      await rootBundle.loadString("assets/hafs_smart_v8.json");
+  final data = json.decode(response);
+  arabic = data["quran"];
+  malayalam = data["malayalam"];
+  return quran = [arabic, malayalam];
 }
